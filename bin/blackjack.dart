2a9efb277c8509d346de8071612308ee84f60a1f
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-08 15:41:48 +0000

    Initial commit

---
+++ bin/blackjack.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:core';
+import 'dart:math';
+import 'package:collection/collection.dart';
+
+void main(List<String> arguments){
+
+
+
+
+
+
+
+}
---
+++ test/blackjack_test.dart
@@ -0,0 +1,8 @@
+import 'package:blackjack/blackjack.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
