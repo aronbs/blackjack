9320defa536330b7592aa103819f80e630f86e10
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-08 15:56:12 +0000

    Making a deck
    Making a deck and then drawing randomly two cards from it

--- bin/blackjack.dart
+++ bin/blackjack.dart
@@ -6,6 +6,17 @@ import 'package:collection/collection.dart';
 
 void main(List<String> arguments){
 
+List<String> deck = ['1','1','1','1','2','2','2','2','3','3','3','3','4','4','4','4','5','5','5','5','6','6','6','6','7','7','7','7','8','8','8','8','9','9','9','9','10','10','10','10','gosi','gosi','gosi','gosi','drottning','drottning','drottning','drottning','kongur','kongur','kongur','kongur'];
+List<String> drawnCards= [];
+Random r = new Random();
+int random = 0;
+
+while(drawnCards.length != 2) {
+  random = r.nextInt(deck.length);
+  drawnCards.add(deck[random]);
+  deck.removeAt(random);
+}
+print(drawnCards.join(' og '));
 
 
 
