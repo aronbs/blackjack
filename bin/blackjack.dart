b6f167534ea1016edb8cc1138f7a4f262b62484e
aronbs <101139622+aronbs@users.noreply.github.com>
2022-03-08 16:36:08 +0000

    hit or stay added
    adding more cards from the deck if i want to hit, and finally when i stay, it removes gosi, drottning and kongur from the drawnCards and adds '10' instead so i can find the sum

--- bin/blackjack.dart
+++ bin/blackjack.dart
@@ -6,17 +6,58 @@ import 'package:collection/collection.dart';
 
 void main(List<String> arguments){
 
-List<String> deck = ['1','1','1','1','2','2','2','2','3','3','3','3','4','4','4','4','5','5','5','5','6','6','6','6','7','7','7','7','8','8','8','8','9','9','9','9','10','10','10','10','gosi','gosi','gosi','gosi','drottning','drottning','drottning','drottning','kongur','kongur','kongur','kongur'];
-List<String> drawnCards= [];
-Random r = new Random();
-int random = 0;
+  List<String> deck = ['1','1','1','1','2','2','2','2','3','3','3','3','4','4','4','4','5','5','5','5','6','6','6','6','7','7','7','7','8','8','8','8','9','9','9','9','10','10','10','10','gosi','gosi','gosi','gosi','drottning','drottning','drottning','drottning','kongur','kongur','kongur','kongur'];
+  List<String> drawnCards= [];
+  Random r = new Random();
+  int random = 0;
+  var sum = 0;
+  bool done = true;
+  while(drawnCards.length != 2) {
+    random = r.nextInt(deck.length);
+    drawnCards.add(deck[random]);
+    deck.removeAt(random);
+  }
+  print(drawnCards.join(' og '));
 
-while(drawnCards.length != 2) {
-  random = r.nextInt(deck.length);
-  drawnCards.add(deck[random]);
-  deck.removeAt(random);
-}
-print(drawnCards.join(' og '));
+
+
+  print('Would you like to hit or stay?');
+  String action = stdin.readLineSync();
+  while(action.toLowerCase() != 'stay')
+    {
+      random = r.nextInt(deck.length);
+      drawnCards.add(deck[random]);
+      deck.removeAt(random);
+      print(drawnCards.join(' og '));
+      print('Would you like to hit or stay?');
+      action = stdin.readLineSync();
+    }
+
+    while(done != false) {
+      if (drawnCards.contains('gosi')) {
+        drawnCards.remove('gosi');
+        drawnCards.add('10');
+      }
+
+      else if (drawnCards.contains('drottning')) {
+        drawnCards.remove('drottning');
+        drawnCards.add('10');
+      }
+
+      else if (drawnCards.contains('kongur')) {
+        drawnCards.remove('kongur');
+        drawnCards.add('10');
+      }
+
+      else {
+        for (int i = 0; i < drawnCards.length; i++) {
+          sum += int.parse(drawnCards[i]);
+          done = false;
+        }
+      }
+    }
+
+    print(sum);
 
 
 
